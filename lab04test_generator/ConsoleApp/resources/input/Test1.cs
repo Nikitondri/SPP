028526393d0815a6482945adbecb5f29b960c475
Nikita Zakharenko <nikita.zakharenko@idftechnology.by>
2022-11-26 11:15:50 +0300

    add body to methods

--- lab04test_generator/ConsoleApp/resources/input/Test1.cs
+++ lab04test_generator/ConsoleApp/resources/input/Test1.cs
@@ -4,18 +4,22 @@
     {
         public void Method1()
         {
+            Console.WriteLine("hello world");
         }
         
         private void Method2()
         {
+            Console.WriteLine("hello world");
         }
         
         public void Method3()
         {
+            Console.WriteLine("hello world");
         }
         
         public void Method4()
         {
+            Console.WriteLine("hello world");
         }
 	}
 }
