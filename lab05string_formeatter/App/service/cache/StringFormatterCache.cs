afd5474dcef4526a635978849a85a3667842ecb9
Nikita Zakharenko <nikita.zakharenko@idftechnology.by>
2022-12-10 16:58:21 +0300

    add cache

---
+++ lab05string_formeatter/App/service/cache/StringFormatterCache.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+namespace App.service.cache;
+
+public class StringFormatterCache
+{
+    private readonly ConcurrentDictionary<string, Func<object, string>> _cache = new();
+
+    public bool IsExistElement(string key)
+    {
+        return _cache.ContainsKey(key);
+    }
+
+    public void AddElement(string key, Func<object, string> value)
+    {
+        _cache.TryAdd(key, value);
+    }
+
+    public Func<object, string> GetElement(string key)
+    {
+        return _cache[key];
+    }
+}
