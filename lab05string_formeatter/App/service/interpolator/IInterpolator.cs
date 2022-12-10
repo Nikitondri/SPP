ecfc0d234b3daedaf22d61b64b54b3df9348dce5
Nikita Zakharenko <nikita.zakharenko@idftechnology.by>
2022-12-10 18:30:12 +0300

    Add interpolator

---
+++ lab05string_formeatter/App/service/interpolator/IInterpolator.cs
@@ -0,0 +1,6 @@
+﻿namespace App.service.interpolator;
+
+public interface IInterpolator
+{
+    string Interpolate(string value, object obj);
+}
---
+++ lab05string_formeatter/App/service/interpolator/InterpolatorImpl.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using App.service.cache;
+
+namespace App.service.interpolator;
+
+public class InterpolatorImpl : IInterpolator
+{
+    private readonly StringFormatterCache _cache = new();
+
+    public string Interpolate(string value, object obj)
+    {
+        var field = obj.GetType() + "." + value;
+        return _cache.IsExistElement(field)
+            ? InterpolateExistFunc(field, obj)
+            : InterpolateNotExistFunc(value, field, obj);
+    }
+
+    private string InterpolateExistFunc(string field, object obj)
+    {
+        return _cache.GetElement(field)(obj);
+    }
+
+    private string InterpolateNotExistFunc(string value, string key, object obj)
+    {
+        var param = Expression.Parameter(typeof(object));
+        var field = Expression.PropertyOrField(Expression.TypeAs(param, obj.GetType()), value);
+        var callExpression = Expression.Call(field, "ToString", null, null);
+        var expression = Expression.Lambda<Func<object, string>>(callExpression, param);
+        var result = expression.Compile();
+        _cache.AddElement(key, result);
+        return result(obj);
+    }
+}
